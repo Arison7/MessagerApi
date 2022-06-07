aa432d951f5ab37f0a2963062bd01df78be77eec
Arison <arison646@gmail.com>
2022-06-07 20:10:32 +0200

    delete client file

--- client.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-req = requests.post('http://localhost:8000/endpoint/messages/', 
-                    data={'message': 'Hello World',"chat":'http://localhost:8000/endpoint/chats/2/'},
-                    auth=('admin', 'messageApi#1'))
-print(req.text)
