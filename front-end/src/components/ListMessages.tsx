2b070136e483ae1b491f46174e49e93d7bf11067
Arison <arison646@gmail.com>
2022-08-02 22:04:00 +0200

    rewrote ListMessages component as a class
    will be later use for easier creating of auto scrolling on message
    posted

--- front-end/src/components/ListMessages.tsx
+++ front-end/src/components/ListMessages.tsx
@@ -1,32 +1,49 @@
-import React, {memo, useContext} from "react";
-import {IState as Props} from "../App";
+import React, { Component, memo, useContext } from "react";
+import { IState as Props, IContext as Context } from "../App";
 import MessageInstance from "./MessageInstance";
 import AuthUserContext from "../contexts/AuthUserContext";
 interface IProps {
-    messages: Props['message'][],
+	messages: Props["message"][];
 }
 
+const ListMessagesFunctional: React.FC<IProps> = memo(({ messages }) => {
+	const user = useContext(AuthUserContext);
+	const RenderList = (): JSX.Element[] => {
+		return messages?.map((msg) => {
+			const classname =
+				msg.author === user.url
+					? "list-Messages-item-Author"
+					: "list-Messages-item-Other";
+			return (
+				<li className={classname} key={msg.url.toString()}>
+					<MessageInstance message={msg} />
+				</li>
+			);
+		});
+	};
+	return <ul className="list-Messages">{RenderList()}</ul>;
+});
 
-const ListMessages: React.FC<IProps> = memo(({messages}) => {
-    const user = useContext(AuthUserContext)
-
-    const RenderList = (): JSX.Element[] => {
-        return messages?.map(msg => {
-            const classname = (msg.author === user.url) ? "list-Messages-item-Author" : "list-Messages-item-Other"
-            return (
-                <li className = {classname} key = {msg.url.toString()}>
-                    <MessageInstance message = {msg}/>
-                </li>
-            )
-        }) 
-
-    }
-    return (
-        <ul className="list-Messages" >
-            {RenderList()}
-        </ul>
-    )
-
-})
+class ListMessages extends Component<IProps> {
+	static contextType : React.Context<Context['user']> = AuthUserContext;
+    declare context: React.ContextType<typeof AuthUserContext>;
+	render() {
+		const { messages } = this.props;
+		const RenderList = (): JSX.Element[] => {
+			return messages?.map((msg) => {
+				const classname =
+					msg.author === this.context.url
+						? "list-Messages-item-Author"
+						: "list-Messages-item-Other";
+				return (
+					<li className={classname} key={msg.url.toString()}>
+						<MessageInstance message={msg} />
+					</li>
+				);
+			});
+		};
+		return <ul className="list-Messages">{RenderList()}</ul>;
+	}
+}
 
 export default ListMessages;
